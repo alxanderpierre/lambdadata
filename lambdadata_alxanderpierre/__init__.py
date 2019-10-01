386f91d03ce7e8c5334d06dbd2203ef799a8d369
alxanderpierre <alxanderpierre@users.noreply.github.com>
2019-09-30 20:54:18 -0400

    added some proof of concept utility functions

---
+++ lambdadata_alxanderpierre/__init__.py
@@ -0,0 +1,10 @@
+"""
+lambdadata  - a collection of data science helper functions
+"""
+
+import pandas as pd
+import numpy as np
+
+#sampe code
+ONES = pd.DataFrame(np.ones(10))
+ZERO = pd.DataFrame(np.zeros(50))
---
+++ lambdadata_alxanderpierre/df_utils.py
@@ -0,0 +1,7 @@
+"""
+utility funchtions for working with DataFrames
+"""
+
+import pandas as pd
+
+TEST_DF = pd.DataFrame([1,2,3])
