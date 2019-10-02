570c1c8718383132cc8ed261f47d57f02bf74a14
alxanderpierre <alxanderpierre@users.noreply.github.com>
2019-10-01 20:26:27 -0400

    split functions drop col function and na funtions

--- lambdadata_alxanderpierre/df_utils.py
+++ lambdadata_alxanderpierre/df_utils.py
@@ -1,7 +1,28 @@
 """
 utility funchtions for working with DataFrames
 """
-
-import pandas as pd
 import numpy as np
-TEST_DF = pd.DataFrame([1,2,3,np.nan])
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def null(X):
+  null_list = X.isnull().sum()
+  return null_list
+
+
+
+
+def split(df):
+    train, test =  train_test_split(df, train_size=.75)
+    train, val = train_test_split(train, train_size=.75)
+    print(train, test, val)
+    print(train.shape, test.shape, val.shape)
+
+
+
+
+def dropcol(df):
+    df = df.copy()
+    df= df.drop(columns=[drop_clomuns])
+    return df
